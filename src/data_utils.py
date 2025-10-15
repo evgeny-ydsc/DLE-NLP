38b001f3c141f7f2c2987a691bdacbde1dbd159c
Evgeny S. <evgeny-ydsc@yandex.ru>
2025-10-15 21:42:48 +0300

    project

--- src/data_utils.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from pathlib import Path
-import urllib.request
-
-def download_dataset(raw_dataset_url:str, raw_dataset_path:str):
-    # чтобы не закачивать на github этот датасет, скачаем его (один раз):
-
-    if not os.path.exists(raw_dataset_path):
-        urllib.request.urlretrieve(raw_dataset_url, raw_dataset_path)
